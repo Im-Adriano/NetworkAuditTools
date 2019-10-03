8d4d4b85cc56235d6563e2d359dc70782b28c69d
Im-Adriano <35747937+Im-Adriano@users.noreply.github.com>
2019-10-03 15:48:59 -0400

    Create arpScanner.py

---
+++ arpScanner.py
@@ -0,0 +1,66 @@
+import socket
+import sys
+from struct import pack
+import struct
+import fcntl
+import ipaddress
+
+def get_ip_and_mask(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ip = socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        35093, 
+        struct.pack('256s', ifname[:15].encode('utf-8'))
+    )[20:24])
+    netmask = socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(), 
+        35099, 
+        struct.pack('256s', ifname[:15].encode('utf-8'))
+    )[20:24])
+    mac = fcntl.ioctl(s.fileno(),
+        35111,  
+        struct.pack('256s', ifname[:15].encode('utf-8')))[18:24]
+    return ip, netmask, mac
+
+
+s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(3))
+s.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 1024)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+s.bind((sys.argv[1], 0))
+s.settimeout(.1)
+
+localIP, netmask, mac = get_ip_and_mask(sys.argv[1])
+
+net = ipaddress.ip_network(localIP+'/'+netmask, strict=False)
+
+localIP = socket.inet_aton(str(localIP))
+
+for ip in ipaddress.IPv4Network(net):
+    ip = socket.inet_aton(str(ip))
+
+    ARP_FRAME = [
+        pack('!6B', *(0xFF,)*6),
+        pack('!6B', *mac),
+        pack('!H', 0x0806),
+        pack('!H', 0x0001), # HRD
+        pack('!H', 0x0800), # PRO
+        pack('!B', 0x06), # HLN
+        pack('!B', 0x04), # PLN 
+        pack('!H', 0x0001), # OP
+        pack('!6B', *mac), # SHA
+        pack('!4B', *localIP), # SPA
+        pack('!6B', *(0x00,)*6), # THA
+        pack('!4B', *ip), # TPA
+    ]
+
+    s.send(b''.join(ARP_FRAME))
+    try:
+        ret = s.recv(1024)
+        if ret[12:14] == b'\x08\x06' and ret[20:22] == b'\x00\x02':
+            foundIP = ipaddress.IPv4Address(ret[28:32])
+            foundMAC = ':'.join('%02x' % b for b in ret[22:28]).upper()
+            print(f'{foundIP} found at {foundMAC}')
+    except socket.timeout:
+        pass
+ 
+
